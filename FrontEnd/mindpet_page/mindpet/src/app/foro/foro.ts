58eed0eedbeaa94e6f1e8a0cc586d9015bc96ada
mindpet0-lang <mindpet0@gmail.com>
2026-03-12 12:14:46 -0500

    f-foro-c

--- FrontEnd/mindpet_page/mindpet/src/app/foro/foro.ts
+++ FrontEnd/mindpet_page/mindpet/src/app/foro/foro.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-foro',
@@ -7,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrl: './foro.css',
 })
 export class Foro {
+postForm: FormGroup;
 
+  posts:any[] = [];
+
+  constructor(private fb:FormBuilder){
+
+    this.postForm = this.fb.group({
+      contenido:['',Validators.required]
+    });
+
+  }
+
+  crearPost(){
+
+    if(this.postForm.invalid) return;
+
+    const nuevoPost = {
+
+      usuario:"Usuario",
+      fecha:new Date().toLocaleDateString(),
+      contenido:this.postForm.value.contenido,
+      likes:0,
+      comentarios:[],
+      nuevoComentario:''
+
+    };
+
+    this.posts.unshift(nuevoPost);
+
+    this.postForm.reset();
+
+  }
+
+  darLike(post:any){
+
+    post.likes++;
+
+  }
+
+  agregarComentario(post:any){
+
+    if(!post.nuevoComentario) return;
+
+    post.comentarios.push({
+
+      usuario:"Usuario",
+      texto:post.nuevoComentario
+
+    });
+
+    post.nuevoComentario='';
+
+  }
 }
