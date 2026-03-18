a5acdf917b612750d8f1ac08c0af9b672712d040
mindpet0-lang <mindpet0@gmail.com>
2026-03-18 09:14:18 -0500

    java

---
+++ Backend/src/test/java/com/example/mindpet/MindPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
