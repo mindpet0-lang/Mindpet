a652eba9fd8ee5c581649a2481492523929ee2b2
mindpet0-lang <mindpet0@gmail.com>
2026-03-18 08:22:19 -0500

    Delete backend/demo (1)/demo directory

--- backend/demo (1)/demo/src/main/java/com/example/mindPet/Repository/MascotaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mindPet.Repository;
-
-import com.example.mindPet.Model.Mascota;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MascotaRepository extends JpaRepository<Mascota, Long>  {
-}
